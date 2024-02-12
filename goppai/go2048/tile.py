ea635d8b2cf536c5ef5269526c3f916b35a76d1d
Isaac Antwi <prosperokay@gmail.com>
2024-02-12 17:02:31 +0000

    feat: add board structure

--- goppai/go2048/tile.py
+++ goppai/go2048/tile.py
@@ -10,6 +10,7 @@ class Tile:
     def setvalue(self, value):
         """set tile value"""
         self.__value = value
+        return self
 
     def getvalue(self):
         """get tile value"""
@@ -21,24 +22,10 @@ class Tile:
             tile.setvalue(self.__value)
             self.__value = 0
             return self.__value
-        elif tile.getvalue() == self.__value:
+
+        if tile.getvalue() == self.__value:
             tile.setvalue(self.__value + tile.getvalue())
             self.__value = 0
             return tile.getvalue()
+
         return -1
-
-
-tile_list = [Tile() for _ in range(4)]
-tile_list[1].setvalue(4)
-tile_list[0].setvalue(2)
-
-print([_.getvalue() for _ in tile_list])
-
-lennn = len(tile_list)
-
-for i in range(lennn):
-    lennn -= 1
-    tile_list[lennn - 1].move_to(tile_list[lennn])
-
-
-print([_.getvalue() for _ in tile_list])
